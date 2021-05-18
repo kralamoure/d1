3f411d09afcf231ae39dc90e9d2fb066c85fe1d9
Raúl Garrido <me@happybydefault.com>
2021-05-18 10:36:42 -0400

    remove logging

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
-	github.com/happybydefault/logging v0.0.0-20210507180050-0f3842239c0e
 	github.com/kralamoure/retroutil v0.0.0-20210518132922-a957c67f4004
 	github.com/stretchr/testify v1.5.1 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- retrosvc/retrosvc.go
+++ retrosvc/retrosvc.go
@@ -3,34 +3,26 @@ package retrosvc
 import (
 	"errors"
 
-	"github.com/happybydefault/logging"
-
 	"github.com/kralamoure/retro"
 )
 
 type Config struct {
 	GameServerId int
 	Storer       retro.Storer
-	Logger       logging.Logger
 }
 
 type Service struct {
 	gameServerId int
 	storer       retro.Storer
-	logging      logging.Logger
 }
 
 func NewService(c Config) (*Service, error) {
 	if c.Storer == nil {
 		return nil, errors.New("nil storer")
 	}
-	if c.Logger == nil {
-		c.Logger = logging.Noop{}
-	}
 	svc := &Service{
 		gameServerId: c.GameServerId,
 		storer:       c.Storer,
-		logging:      c.Logger,
 	}
 	return svc, nil
 }
