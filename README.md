a4b1f4842c56799be91c38020a62c193a2f7cbac
Raúl Garrido <me@happybydefault.com>
2021-05-24 16:55:13 -0400

    docs: edit package description

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # retro
 
-`retro` is a core library for Dofus Retro.
+`retro` is a core library for Dofus Retro. It declares data types and provides constants and functions to work with the
+game. It also declares the Storer interface implemented in package [retropg](https://github.com/kralamoure/retropg).
 
 ## Requirements
 
--- retro.go
+++ retro.go
@@ -1,4 +1,7 @@
-// Package retro is a core library for Dofus Retro.
+// Package retro is a core library for Dofus Retro. It declares data types and
+// provides constants and functions to work with the game. It also declares the
+// Storer interface implemented in package retropg
+// (https://github.com/kralamoure/retropg).
 package retro
 
 import (
